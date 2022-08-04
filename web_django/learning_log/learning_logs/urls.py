42c302859e1d527d2566c173bc24ed9fe2c6f8e4
Walker <luowenkuo@hotmail.com>
2022-08-04 15:53:10 +0800

    Topic页

--- web_django/learning_log/learning_logs/urls.py
+++ web_django/learning_log/learning_logs/urls.py
@@ -3,14 +3,15 @@ from django import apps
 from django.urls import URLPattern, path
 from . import views
 
-apps.name='learning_logs'
+apps.name = 'learning_logs'
 
 """
 为每个试图定义url：path('url', 'view', '别名')
 识别到匹配的url，调用相应的view
-""" 
-urlpatterns = [ 
+"""
+urlpatterns = [
     # 主页
-    path('',views.index,name='index'), 
-    
+    path('', views.index, name='index'),
+    path('topics', views.topics, name='topics')
+
 ]
--- web_django/learning_log/learning_logs/views.py
+++ web_django/learning_log/learning_logs/views.py
@@ -1,4 +1,6 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -6,3 +8,9 @@ from django.shortcuts import render
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
